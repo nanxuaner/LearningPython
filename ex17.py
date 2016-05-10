f4d71d35bb1027bb0ad751bb58990c821cbac99a
Anna <test@gmail.com>
2016-05-10 11:54:24 -0700

    add file

--- ex17.py
+++ ex17.py
@@ -6,6 +6,7 @@ script, from_file, to_file = argv
 print "Copying from %s to %s" % (from_file, to_file)
 
 # we chould do these two on one line, how?
+#indata = open(from_file).read()
 in_file = open(from_file)
 indata = in_file.read()
 
@@ -15,6 +16,7 @@ print "Does the output file exist? %r" % exists(to_file)
 print "Ready, hit RETURN to continue, CTRL-C to abort."
 raw_input()
 
+#out_file = open(to_file, 'w').write(indata)
 out_file = open(to_file, 'w')
 out_file.write(indata)
 
@@ -22,3 +24,7 @@ print "Alright, all done."
 
 out_file.close()
 in_file.close()
+
+#the whole code can be shorten by the following statement
+#from shutil import copyfile
+#copyfile(src, dst)
