e5756c28266ccfb6d1426ae8a5b7a4ceec8e60d5
Anna <test@gmail.com>
2016-05-09 18:20:30 -0700

    add ex17.py

--- ex16_RdWtFiles.py
+++ ex16_RdWtFiles.py
@@ -3,7 +3,7 @@ from sys import argv
 script, filename = argv
 
 print "We're going to erase %r." % filename
-print "If you don't want that, hit Ctrl-C (^C)."
+print "If you don't want that, hit CTRL-C (^C)."
 print "If you do want that, hit RETURN."
 
 raw_input("?")
@@ -21,6 +21,9 @@ line2 = raw_input("line 2: ")
 line3 = raw_input("line 3: ")
 
 print "I'm going to write these to the file."
+# x = "%r \n %r \n %r \n" % (line1, line2, line3)
+#
+# target.write(x)
 target.write(line1)
 target.write("\n")
 target.write(line2)
